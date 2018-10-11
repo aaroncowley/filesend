2ce5eb20126f7d50b532babb725117ba464c3a65
aaron <acow777@gmail.com>
2018-10-11 17:10:30 -0600

    menu added, compiles but still mysteriously broken

--- hash.c
+++ hash.c
@@ -8,23 +8,34 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <openssl/md5.h>
+#include <netdb.h> 
+#include <sys/stat.h>
+#include <arpa/inet.h> 
 
 #define DIE(x) { perror(x); exit(EXIT_FAILURE);}
 
-char *md5_check(char *file){
+
+/* ooh colors */
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+void md5_check(char *hash, char *file){
     unsigned char c[MD5_DIGEST_LENGTH];
     
     int i;
-    char hash[33];
     FILE *inFile = fopen(file, "rb");
     MD5_CTX mdContext;
     int bytes;
     unsigned char data[1024];
     
-    if (inFile == NULL){
-        printf("%s can't be opened.\n", file);
-        return 0;
-    }
+    if (inFile == NULL)
+        DIE("ERROR: file cannot be opened for hashing");
 
     MD5_Init(&mdContext);
     while ((bytes = fread(data, 1, 1024, inFile)) != 0)
@@ -32,54 +43,53 @@ char *md5_check(char *file){
     
     MD5_Final(c, &mdContext);
     for(i = 0; i < MD5_DIGEST_LENGTH; i++){
-        printf("%02x", c[i]);
         sprintf(&hash[i*2], "%02x", (unsigned int)c[i]);
     }
 
-    fclose(inFile);
-    printf("Hash: %s\n", hash);
-    return *hash;
+    fclose(inFile); 
 }
 
-void *server(char *file, int port){
+void *server(char *file, int port, char *server_ip){
     int fd, sock, sent_bytes, offset, peer_sock;
     ssize_t len;
+    FILE *serveFile;
     struct sockaddr_in sin;
     struct sockaddr_in peer;
     int opt;
     struct stat file_stat;
     char *filesize;
+    int remaining_bytes;
 
     if ((fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
         DIE("socket failure");
     
     sin.sin_family = AF_INET;
     sin.sin_port = htons(port);
-    inet_pton(AF_INET, host, &sin.sin_addr.s_addr);
+    inet_pton(AF_INET, server_ip, &sin.sin_addr.s_addr);
     
     sock = socket(AF_INET, SOCK_STREAM, 0);
 
     if (sock == -1)
         DIE("ERROR: socket()");
 
-    if (bind(sock, (struct sock_addr *)&sin, sizeof(struct sockaddr_in)) == -1)
+    if (bind(sock, (struct sockaddr *)&sin, sizeof(struct sockaddr_in)) == -1)
         DIE("ERROR: bind()");
     
     if (listen(sock, 5) == -1)
         DIE("ERROR: listen()");
 
-    fd = open(file, O_RDONLY);
+    serveFile = fopen(file, "r");
     if (fd == -1)
         DIE("ERROR: opening file");
     
-    if (fstat(fd, &file_stat) < 0)
+    if (fstat(serveFile, &file_stat) < 0)
         DIE("ERROR: fstat");
     printf("File Size: %d bytes\n", file_stat.st_size);
 
     peer_sock = accept(sock, (struct sockaddr *)&peer, sizeof(struct sockaddr));
-    if (peer == -1)
+    if (peer_sock == -1)
         DIE("ERROR: accept()");
-    printf("Peer accpeted: %s\n", inet_ntop(peer));
+    printf("Peer accpeted: %s\n", inet_ntoa(peer.sin_addr));;
     
     sprintf(filesize, "%d", file_stat.st_size);
     /*send size of file*/
@@ -87,18 +97,17 @@ void *server(char *file, int port){
     if (len < 0)
         DIE("ERROR: On handshake");
     printf("Sent %d bytes as filesize\n", len);
-
+    
     /* Sending file data */
-    while (((sent_bytes = sendfile(peer_sock, fd, &offset, BUFSIZ)) > 0) && (remain_data > 0)){
-        fprintf(stdout, "1. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remain_data);
-        remain_data -= sent_bytes;
-        fprintf(stdout, "2. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remain_data);
+    while (((sent_bytes = send(peer_sock, serveFile, &offset, BUFSIZ)) > 0) && (remaining_bytes> 0)){
+        printf("1. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remaining_bytes);
+        remaining_bytes -= sent_bytes;
+        printf("2. Server sent %d bytes from file's data, offset is now : %d and remaining data = %d\n", sent_bytes, offset, remaining_bytes);
     }
 
     close(peer_sock);
-    close(sock);
-
-    return 0;
+    close(sock); 
+    printf("File sent, socket closed.");
 }
 
 void *client(char *file, int port, char *server_ip){
@@ -145,13 +154,55 @@ void *client(char *file, int port, char *server_ip){
     }
     fclose(received_file);
     close(sock);
-    return 0;
 }
 
 int main(int argc, char *argv[]){
-    char *filename = argv[1];
-    char *hash = md5_check(filename);
-    printf("the hash %s \n", hash);
+    char *filename;
+    char *port;
+    char *hash;
+    int command, hostname;
+    char *IPbuf;
+    char *hostbuffer;
+    struct hostent *host_entry;
+
+    printf(RED"\n\n----------------pick an option----------------\n"RESET);
+    printf(CYAN"option s: send a file\n"RESET);
+    printf(CYAN"option h: hash a file\n"RESET);
+    printf(CYAN"option r: receive a file\n"RESET);
+    printf(RED"----------------------------------------------\n\n"RESET);
+
+    do {
+        printf(GREEN"Enter your Command >>> "RESET);
+        command = getchar();
+        switch(command){
+            case 's':
+                hostname = gethostname(hostbuffer, sizeof(hostbuffer));
+                if (hostname == -1)
+                    DIE("ERROR: Could Not get hostname");
+
+                host_entry = gethostbyname(hostbuffer);
+                if (host_entry == -1)
+                    DIE("ERROR: Could not retrieve host info");
+                
+                IPbuf = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+                printf("using IP: %s\n", IPbuf);
+
+                printf("enter a filename: ");
+                fgets(filename, 30, stdin);
+                printf("\n");
+                
+                printf("enter a port: ");
+                fgets(port, 6, stdin);
+                printf("\n");
+
+                printf("starting server on socket %s:%s \n", IPbuf, port);
+                server(filename, port, IPbuf); 
+        }
+    } while(1);
+
+    //md5_check(hash, filename);
+    //printf("the hash %s \n", hash);
+        
     return 0;
 }
 
